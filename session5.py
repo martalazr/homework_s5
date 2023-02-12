e7b963a3e0b66c92717d00a7d8c204421f5d4dcb
marta <mlazaro.ieu2021@student.ie.edu>
2023-02-12 20:23:53 +0100

    Homework for Session 5

---
+++ session5.py
@@ -0,0 +1,24 @@
+print("Hello! Let's calculate your net salary")
+
+try:
+    gross = float(input("What is your gross salary? "))
+    children = int(input("How many children do you have? "))
+except ValueError:
+    print("Your input was invalid. Try again. ")
+else:
+    if gross < 1000:
+        tax = 0.1 * gross
+    elif gross < 2000:
+        tax = 0.12 * gross
+        tax_cut = 0.01 * children * gross
+    elif gross < 4000:
+        tax = 0.14 * gross
+        tax_cut = 0.01 * children * gross
+    else:
+        tax = 0.18 * gross
+        tax_cut = 0.005 * children * gross
+
+
+net = gross + tax_cut - tax
+print("The net salary is:", net)
+
